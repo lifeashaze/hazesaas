968e2ee7b746628389887d84e5da63a79a6a20a7
lifeashaze <34096693+lifeashaze@users.noreply.github.com>
2024-02-24 20:14:11 +0530

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import { ThemeToggle } from "./components/ThemeToggle";
+import { Button } from "@/components/ui/button";
+import {RegisterLink, LoginLink, LogoutLink} from "@kinde-oss/kinde-auth-nextjs/components";
+
+export default function Home() {
+  return (
+    <>
+      <section className="flex items-center bg-background h-[90vh]">
+        <div className="relative items-center w-full px-5 py-12 mx-auto lg:px-16 max-w-7xl md:px-12">
+          <div className="max-w-3xl mx-auto text-center">
+            <div>
+              <span className="w-auto px-6 py-3 rounded-full bg-secondary">
+                <span className="text-sm font-medium text-primary"> Sort your notes easily</span>
+              </span>
+              <h1 className="mt-8 text-5xl font-extrabold tracking-tight lg:text-6xl">Create Notes with Ease</h1>
+              <p className="max-w-xl mx-auto mt-8 text-base lg:text-xl text-secondry-foreground">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente labore maxime explicabo, tenetur non necessitatibus, ipsam accusantium nostrum optio iusto eligendi?
+              </p>
+            </div>
+            <div className="flex justify-center max-w-sm mx-auto mt-10">
+              <RegisterLink><Button size="lg" className="w-full">Sign up for Free</Button></RegisterLink>
+
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
