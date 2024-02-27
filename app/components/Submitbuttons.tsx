023b6c9a4cb2778ab82eb6d3c5d328d7c805df11
lifeashaze <34096693+lifeashaze@users.noreply.github.com>
2024-02-27 22:51:34 +0530

    added stripe webhook  and fixed redirection bug

--- app/components/Submitbuttons.tsx
+++ app/components/Submitbuttons.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { Loader, Loader2 } from "lucide-react";
+import { Loader2, Trash } from "lucide-react";
 import { useFormStatus } from "react-dom";
 
 export function SubmitButton() {
@@ -13,25 +13,64 @@ export function SubmitButton() {
           <Loader2 className="mr-2 w-4 h-4 animate-spin" /> Please Wait
         </Button>
       ) : (
-        <Button className="w-fit" type="submit">Save Now</Button>
+        <Button className="w-fit" type="submit">
+          Save Now
+        </Button>
       )}
     </>
   );
 }
 
-export function StripeSubscriptionCreationButton(){
-  const {pending} = useFormStatus();
+export function StripeSubscriptionCreationButton() {
+  const { pending } = useFormStatus();
 
   return (
     <>
-    {pending ? (
-      <Button disabled className="w-full">
-        <Loader2 className="mr-2 w-4 h-4 animate-spin" /> Please Wait
-      </Button>
-    ):(
-      <Button type="submit" className="w-full">Create Subscription</Button>
-    )}
+      {pending ? (
+        <Button disabled className="w-full">
+          <Loader2 className="mr-2 w-4 h-4 animate-spin" /> Please Wait
+        </Button>
+      ) : (
+        <Button type="submit" className="w-full">
+          Create Subscription
+        </Button>
+      )}
     </>
-  
-  )
+  );
+}
+
+export function StripePortal() {
+  const { pending } = useFormStatus();
+
+  return (
+    <>
+      {pending ? (
+        <Button disabled className="w-fit">
+          <Loader2 className="mr-2 w-4 h-4 animate-spin" /> Please Wait
+        </Button>
+      ) : (
+        <Button className="w-fit" type="submit">
+          View payment details
+        </Button>
+      )}
+    </>
+  );
+}
+
+export function TrashDelete() {
+  const { pending } = useFormStatus();
+
+  return (
+    <>
+      {pending ? (
+        <Button variant={"destructive"} size="icon" disabled>
+          <Loader2 className="h-4 w-4 animate-spin" />
+        </Button>
+      ) : (
+        <Button variant={"destructive"} size="icon" type="submit">
+          <Trash className="h-4 w-4" />
+        </Button>
+      )}
+    </>
+  );
 }
