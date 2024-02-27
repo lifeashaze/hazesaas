89fbf4a34f76d8e7e50a2f658903441b79622402
lifeashaze <34096693+lifeashaze@users.noreply.github.com>
2024-02-27 23:09:49 +0530

    settings fix

--- app/dashboard/settings/page.tsx
+++ app/dashboard/settings/page.tsx
@@ -1,5 +1,3 @@
-import { SubmitButton } from "@/app/components/Submitbuttons";
-import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -10,71 +8,77 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
-import {getKindeServerSession} from "@kinde-oss/kinde-auth-nextjs/server";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import prisma from "@/lib/db";
-import { revalidatePath } from "next/cache";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-async function getData(userId: string){
-    const data = await prisma?.user.findUnique({
-        where:{
-            id: userId,
-        },
-        select : {
-            name: true,
-            email: true,
-            colorScheme: true
-        }
-    });
-    return data;
+import { SubmitButton } from "@/app/components/Submitbuttons";
+import { revalidatePath, unstable_noStore as noStore } from "next/cache";
+
+async function getData(userId: string) {
+  noStore();
+  const data = await prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+    select: {
+      name: true,
+      email: true,
+      colorScheme: true,
+    },
+  });
+
+  return data;
 }
 
 export default async function SettingPage() {
-    const {getUser} = getKindeServerSession()
-    const user = await getUser()
-    const data = await getData(user?.id as string)
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  const data = await getData(user?.id as string);
 
-    async function postData(formData: FormData){
-        "use server";
+  async function postData(formData: FormData) {
+    "use server";
 
-        const name = formData.get("name") as string;
-        const colorScheme = formData.get("color") as string;
+    const name = formData.get("name") as string;
+    const colorScheme = formData.get("color") as string;
 
-        await prisma.user.update({
-            where: {
-                id: user?.id,
-            },
-            data: {
-                name: name ?? undefined,
-                colorScheme: colorScheme ?? undefined,
-            }
-                
+    await prisma.user.update({
+      where: {
+        id: user?.id,
+      },
+      data: {
+        name: name ?? undefined,
+        colorScheme: colorScheme ?? undefined,
+      },
+    });
 
-            
-        })
-
-        revalidatePath('/', "layout")
-
-    }
-
-
-
+    revalidatePath("/", "layout");
+  }
 
   return (
     <div className="grid items-start gap-8">
       <div className="flex items-center justify-between px-2">
         <div className="grid gap-1">
           <h1 className="text-3xl md:text-4xl">Settings</h1>
-          <p className="text-lg text-muted-foreground">Your Profile Settings</p>
+          <p className="text-lg text-muted-foreground">Your Profile settings</p>
         </div>
       </div>
+
       <Card>
         <form action={postData}>
           <CardHeader>
             <CardTitle>General Data</CardTitle>
             <CardDescription>
-              Please provide general information about yourself. Please do not
-              forget to save!
+              Please provide general information about yourself. Please dont
+              forget to save
             </CardDescription>
           </CardHeader>
           <CardContent>
@@ -90,12 +94,12 @@ export default async function SettingPage() {
                 />
               </div>
               <div className="space-y-1">
-                <Label>Your E-Mail</Label>
+                <Label>Your Email</Label>
                 <Input
                   name="email"
                   type="email"
                   id="email"
-                  placeholder="Your E-Mail"
+                  placeholder="Your Email"
                   disabled
                   defaultValue={data?.email as string}
                 />
@@ -104,23 +108,22 @@ export default async function SettingPage() {
               <div className="space-y-1">
                 <Label>Color Scheme</Label>
                 <Select name="color" defaultValue={data?.colorScheme}>
-                    <SelectTrigger className="w-full">
-                        <SelectValue placeholder="Select a color" />
-                    </SelectTrigger>
-                    <SelectContent>
-                        <SelectGroup>
-                            <SelectLabel>Color</SelectLabel>
-                            <SelectItem value="theme-green">Green</SelectItem>
-                            <SelectItem value="theme-blue">Blue</SelectItem>
-                            <SelectItem value="theme-violet">Violet</SelectItem>
-                            <SelectItem value="theme-yellow">Yellow</SelectItem>
-                            <SelectItem value="theme-orange">Orange</SelectItem>
-                            <SelectItem value="theme-red">Red</SelectItem>
-                            <SelectItem value="theme-rose">Rose</SelectItem>
-                        </SelectGroup>
-                    </SelectContent>
+                  <SelectTrigger className="w-full">
+                    <SelectValue placeholder="Select a color" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectLabel>Color</SelectLabel>
+                      <SelectItem value="theme-green">Green</SelectItem>
+                      <SelectItem value="theme-blue">Blue</SelectItem>
+                      <SelectItem value="theme-violet">Violet</SelectItem>
+                      <SelectItem value="theme-yellow">Yellow</SelectItem>
+                      <SelectItem value="theme-orange">Orange</SelectItem>
+                      <SelectItem value="theme-red">Red</SelectItem>
+                      <SelectItem value="theme-rose">Rose</SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
                 </Select>
-
               </div>
             </div>
           </CardContent>
